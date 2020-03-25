7b5168c646c352be272139c26ec599c34aefdeec
Ian Clough <ian.clough@magiaconsulting.com>
2020-03-25 11:39:55 +0000

    Don't force HTTPS

--- DataService/Startup.cs
+++ DataService/Startup.cs
@@ -39,7 +39,7 @@ namespace DataService
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+ //           app.UseHttpsRedirection();
 
             app.UseRouting();
 
